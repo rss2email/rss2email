e50f06df907da764d787ddb9698d5cca2757c100
W. Trevor King <wking@tremily.us>
2013-01-09 10:43:24 -0500

    error: Strip trailing whitespace in module docstring
    Signed-off-by: W. Trevor King <wking@tremily.us>

--- rss2email/error.py
+++ rss2email/error.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License along with
 # rss2email.  If not, see <http://www.gnu.org/licenses/>.
 
-"""rss2email-specific errors 
+"""rss2email-specific errors
 """
 
 from . import LOG as _LOG
