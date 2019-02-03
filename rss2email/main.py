1fe49a732f4e19578e1eabd894a1a68707bd2907
Profpatsch <mail@profpatsch.de>
2019-02-03 12:56:41 +0100

    Revert "Change from_email subcommand (add) into from-email to be consistent"
    This reverts commit b0398b6bf2309ab65125566a8ee7167c840dcbcc.

--- rss2email/main.py
+++ rss2email/main.py
@@ -89,7 +89,7 @@ def run(*args, **kwargs):
         'email', nargs='?',
         help='target email for the new feed')
     add_parser.add_argument(
-        'from-email', nargs='?',
+        'from_email', nargs='?',
         help='override from field in sent email')
 
     run_parser = subparsers.add_parser(
