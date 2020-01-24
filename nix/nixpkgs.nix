5f4dea6154b33943589cc12ca5bff6934b48ebe4
Profpatsch <mail@profpatsch.de>
2020-01-24 20:44:14 +0100

    nix/nixpkgs: push to current version of 19.09

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,5 +1,5 @@
 builtins.fetchTarball {
-  # nixpkgs 19.09 2019-09-14
-  url = "https://github.com/NixOS/nixpkgs/archive/936a53ee699d9a1db62d9fe3929c2efdc172b833.tar.gz";
-  sha256 = "0yd8n7yasag2sasj48yvr6rzaqdq75rsvlfnaw55nc5bpwh7s6ni";
+  # nixpkgs 19.09 2020-01-24
+  url = "https://github.com/NixOS/nixpkgs/archive/e6391b4389e10a52358bd94b3031238648818b0a.tar.gz";
+  sha256 = "07nkaihks0k60krghd900x8zhfrsxmvdchlf5fzqmhgmfw5x6krv";
 }
