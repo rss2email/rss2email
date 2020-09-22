2143137913690450446d36feceebfb5bff4e3376
Léo Gaspard <leo@gaspard.io>
2020-09-23 00:43:03 +0200

    Bump nixpkgs

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,5 +1,5 @@
 builtins.fetchTarball {
-  # nixpkgs 19.09 2020-01-24
-  url = "https://github.com/NixOS/nixpkgs/archive/e6391b4389e10a52358bd94b3031238648818b0a.tar.gz";
-  sha256 = "07nkaihks0k60krghd900x8zhfrsxmvdchlf5fzqmhgmfw5x6krv";
+  # nixpkgs 20.09 2020-09-23
+  url = "https://github.com/NixOS/nixpkgs/archive/3b8ddb2f1ee6f4c0794fb6dfbd273c3599492b76.tar.gz";
+  sha256 = "06hgvyd8ry4i49dmjxh5n6wv1j5ifpp7i3a7bjz62san0q6d0j35";
 }
