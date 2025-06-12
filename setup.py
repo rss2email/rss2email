c9ca07ed401f92b6e44c101536e5dac55e60140d
Sebastian Pipping <sebastian@pipping.org>
2025-06-12 16:03:19 +0200

    Drop support for end-of-life Python <3.9

--- setup.py
+++ setup.py
@@ -56,7 +56,6 @@ setuptools.setup(
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
